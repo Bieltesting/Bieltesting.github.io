bc4436bf3e15c63e93c6f7bf99c5d365205b3ed2
Bieltesting <126650696+Bieltesting@users.noreply.github.com>
2023-03-01 17:58:28 -0300

    one commit

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const botaoSeguir = document.getElementById("botao-seguir");
+const compartilhar = document.getElementById("botao-compartilhar")
 const linkBloqueado = document.getElementById("link-bloqueado");
 const linkDesbloqueado = document.getElementById("link-desbloqueado");
 
 botaoSeguir.addEventListener("click", function() {
+  compartilhar.style.display = "block";
+  
+});
+
+compartilhar.addEventListener("click", function() {
   linkBloqueado.style.display = "none";
   linkDesbloqueado.style.display = "block";
+})
+
+//Adicione um ouvinte de evento ao botão "compartilhar"
+compartilhar.addEventListener('click', function() {
+  //Obtenha a URL atual da página
+  const url = window.location.href;
+
+//Crie a URL do WhatsApp com a mensagem desejada
+const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(`Venha se tornar Promoter você também: ${url}`)}`;
+
+//Abra a URL do WhatsApp em uma nova janela
+window.open(whatsappUrl);
 });
